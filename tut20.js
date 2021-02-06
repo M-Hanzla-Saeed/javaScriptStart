8cd33c082a9a1f6fb47e05f9b0e3b6eafc2d4e9b
Hanzla <mhanzla712@gmail.com>
2021-02-06 23:51:26 +0500

    today work

--- tut20.js
+++ tut20.js
@@ -18,3 +18,43 @@ console.log(Name);
 // // for clear the sotrage entire session...!
 // sessionStorage.clear();
 
+let impArray = ['Adrak', 'pyaz', 'tamatar'];
+localStorage.setItem('sabzi', 'impArray');
+ 
+localStorage.setItem('sabzi', JSON.stringify(impArray));
+
+sabziItem = JSON.parse(localStorage.getItem('sabzi'));
+console.log(sabziItem);
+
+let khasArray = ['pani','roti','salan','chye'];
+localStorage.setItem('khana' , 'khasArray');
+
+localStorage.setItem('khana', JSON.stringify(khasArray));
+
+khanaaa = JSON.parse(localStorage.getItem('khana'));
+console.log(khanaaa);
+
+
+
+// again i'm repeat the same lec for practice::
+
+localStorage.setItem('value', ' Burger=100');
+let why = localStorage.getItem('value');
+console.log(why);
+// localStorage.clear();
+// sessionStorage.clear();
+
+
+
+
+
+
+
+let ARRAY = [12,13,14,15,16,17];
+localStorage.setItem('numberArray', 'ARRAY');
+
+localStorage.setItem('numberArray', JSON.stringify(ARRAY));
+
+thisArray = JSON.parse(localStorage.getItem('numberArray'));
+console.log(thisArray);
+
