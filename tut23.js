ad99068a4355d3027c950ccaa2c3e610b029f7ad
Hanzla <mhanzla712@gmail.com>
2021-02-10 23:35:24 +0500

    js work

---
+++ tut23.js
@@ -0,0 +1,28 @@
+console.log('this is tut 23 for math objectss');
+
+let x = 10;
+let y = 20;
+let z = x+y ;  
+
+
+// exploring the math object
+z = Math;
+z = Math.PI;
+z = Math.E;
+z = Math.ceil(4.8);
+z = Math.round(6.5);
+z = Math.floor(-2.9);
+z = Math.abs(-6677);
+z = Math.sqrt(81);
+z = Math.pow(2, 8);
+z = Math.pow(2 , 5);
+z = Math.min(1,2,3,4,5,6,7,8,9);
+z = Math.max(1,2,3,4,5,6,7,8,9);
+z = Math.random();
+// formula remember for random value in  math object 
+//  a = (0 , 1);
+// a100 = a*100 (0 , 100);
+// a10-100 = 10 + a*(100-10);
+z =Math.ceil(100* Math.random());
+
+console.log(z);
