e367633e5aae3faa175eae68023295b22d043ed1
Hanzla <mhanzla712@gmail.com>
2021-02-13 22:25:54 +0500

    ObjectDateInJs

---
+++ tut24.js
@@ -0,0 +1,43 @@
+console.log('this is tut 24');
+// in this lecture we learn about Date Object: Date And Time in Javascript;
+
+let today = new Date();
+console.log("todayDateIs","andTypeIs",typeof today, today);
+// good scene bro
+
+let otherDate = new Date();
+ otherDate = new Date("12-17-2000");
+ otherDate = new Date("12-17-2000 04:54:08");
+ otherDate = new Date("09/10/2003");
+ otherDate = new Date("09/10/2003");
+ console.log(otherDate);
+
+let a ;
+a = otherDate.getDay()
+a = otherDate.getDate()
+a = otherDate.getMonth()
+a = otherDate.getSeconds()
+a = otherDate.getHours()
+a = otherDate.getTime()
+a = otherDate.getMinutes()
+a = otherDate.setDate(09);
+a = otherDate.setMonth(09);
+a = otherDate.setSeconds(09);
+
+console.log(a);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
